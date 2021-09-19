e5cd86d86055528eb9f38040b53ab2db1e77c8fc
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-19 14:18:53 +0600

    button component.

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+	<button @click="onClink()" :style="{ background: color }" class="btn">
+		{{ text }}
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+	props: ['text', 'color'],
+	methods: {
+		onClink() {
+			console.log('Clicked');
+		}
+	}
+};
+</script>
+
+<style></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,17 @@
 <template>
 	<header>
 		<h1>{{ title }}</h1>
+		<Button text="Add Task" color="green" />
 	</header>
 </template>
 
 <script>
+import Button from './Button.vue';
+
 export default {
 	name: 'Header',
-	props: ['title']
+	props: ['title'],
+	components: { Button }
 };
 </script>
 
