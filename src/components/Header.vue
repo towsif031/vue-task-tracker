a71f61506a5cd727df9e565eb6bbeb64a4d1cd8b
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-19 14:08:44 +0600

    Header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'Header',
+	props: ['title']
+};
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+</style>
