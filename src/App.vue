707cfdca5af8791e2be415298f13f7e9feedc5ef
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-19 14:55:28 +0600

    display tasks component.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
 	<div class="container">
 		<Header title="Task Tracker" />
+		<Tasks :tasks="tasks" />
 	</div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Tasks from './components/Tasks.vue';
 
 export default {
 	name: 'App',
-	components: { Header }
+	components: { Header, Tasks },
+	data() {
+		return {
+			tasks: []
+		};
+	},
+	created() {
+		this.tasks = [
+			{
+				id: '1',
+				text: 'Doctors Appointment',
+				day: 'March 5th at 2:30pm',
+				reminder: true
+			},
+			{
+				id: '2',
+				text: 'Meeting with boss',
+				day: 'March 6th at 1:30pm',
+				reminder: true
+			},
+			{
+				id: '3',
+				text: 'Food shopping',
+				day: 'March 7th at 2:00pm',
+				reminder: false
+			}
+		];
+	}
 };
 </script>
 
---
+++ src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<template>
+	<div v-for="task in tasks" :key="task.id">
+		{{ task.text }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Tasks',
+	props: ['tasks']
+};
+</script>
+
+<style></style>
